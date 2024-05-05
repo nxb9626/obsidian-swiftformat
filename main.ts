18708e429db76b9acccae90c44cef4fa02828d69
nxb9626 <nxb9626@rit.edu>
2024-05-05 15:37:41 -0400

    well it works, but it has problems

--- main.ts
+++ main.ts
@@ -12,161 +12,211 @@ var child_process = require('child_process');
 
 export default class MyPlugin extends Plugin {
 
-	async onload() {
-		this.getExtension()
-		this.registerLintDiagnostics()
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Format Swift code blocks in this note',
-			editorCallback: () => {
-				this.getCodeBlocks()
-			}
-		});
-	}
+  async onload() {
+    this.getExtension()
+    // this.registerLintDiagnostics()
+    this.addCommand({
+      id: 'sample-editor-command',
+      name: 'Format Swift code blocks in this note',
+      editorCallback: () => {
+        this.getCodeBlocks()
+      }
+    });
+  }
 
-	isNode(node: SyntaxNode | undefined): node is SyntaxNode {
-		return (node as SyntaxNode) !== undefined
-	}
+  isNode(node: SyntaxNode | undefined): node is SyntaxNode {
+    return (node as SyntaxNode) !== undefined
+  }
 
 
-	applyFormat(content: string): Promise<string> {
-		const root = '/opt/homebrew/bin/swiftformat'
-		const args = [
-			'stdin',
-			'--quiet',
-			'--swiftversion',
-			'5.10',
-			'--emptybraces',
-			'linebreak',
-			'--wrapcollections',
-			'before-first',
-			'--wraparguments',
-			'before-first',
-		];
-		const options = {
-			'input': content
-		}
-		var sfmt = child_process.spawn(root, args, options);
+  applyFormat(content: string): Promise<string> {
+    if (content.startsWith("rust")) {
+      return this.applyRustFormat(content)
+    }
 
-		// Although .exec() is buffered for us,
-		// we'd have to escape everything we pass in. Boo.
-		var buffer: any[] = []
+    if (content.startsWith("swift")) {
+      return this.applySwiftFormat(content)
+    }
+    else {
+      return new Promise((resolve) => {
+        return content
+      });
+    }
+  }
 
-		sfmt.stdin.write(content);
-		sfmt.stdin.end();
+  applyRustFormat(content: string): Promise<string> {
+    // double check this, ~ might not be ideal
+    content = content.slice(4, content.length)
+    const root = '/Users/noah/.cargo/bin/rustfmt'
+    const args: [] = [];
+    const options = {
+      'input': content
+    }
+    const sfmt = child_process.spawn(root, args, options);
 
-		sfmt.stderr.on('data', (chunk: any) => {
-			new ErrorModal(this.app, `${chunk}`).open()
-		})
+    // Although .exec() is buffered for us,
+    // we'd have to escape everything we pass in. Boo.
+    let buffer: char[] = [..."rust\n"]
 
-		sfmt.stdout.on('data', (chunk: any) => {
-			buffer.push(chunk)
-		});
+    sfmt.stdin.write(content);
+    sfmt.stdin.end();
 
-		return new Promise((resolve) => {
-			sfmt.on('close', (code: any) => {
-				console.log(`closed stream with code ${code}`)
-				resolve(`${buffer.join('')}`)
-			})
-		})
-	}
+    sfmt.stderr.on('data', (chunk: any) => {
+      console.log(`${chunk}`)
+      new ErrorModal(this.app, `${chunk}`).open()
+    })
 
-	async getCodeBlocks() {
+    sfmt.stdout.on('data', (chunk: any) => {
+      console.log(`${chunk}`)
+      buffer.push(chunk)
+    });
 
-		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
-		if (view === null) {
-			return "null view"
-		}
-		const editorView: EditorView = (view.editor as any).cm as EditorView;
-		let cursor = syntaxTree(editorView.state).cursor()
+    return new Promise((resolve) => {
+      sfmt.on('close', (code: any) => {
+        console.log(`closed stream with code ${code}`)
+        resolve(`${buffer.join('')}`)
+      })
+    })
+  }
 
-		// Would use null, but TS still thinks its null even when we check.
-		var startNodePos: number = 0
-		var endNodePos: number = 0
+  applySwiftFormat(content: string): Promise<string> {
+    const root = '/opt/homebrew/bin/swiftformat'
+    const args = [
+      'stdin',
+      '--quiet',
+      '--swiftversion',
+      '5.10',
+      '--emptybraces',
+      'linebreak',
+      '--wrapcollections',
+      'before-first',
+      '--wraparguments',
+      'before-first',
+    ];
+    const options = {
+      'input': content
+    }
+    const sfmt = child_process.spawn(root, args, options);
 
-		// Find code blocks with the lang
-		// TODO: Filter to `swift`
-		cursor.iterate((node: SyntaxNodeRef) => {
-			if (node.name.contains("HyperMD-codeblock-begin")) {
-				startNodePos = node.node.from
-				return false
-			} else if (node.type.name.contains("HyperMD-codeblock-end")) {
-				endNodePos = node.node.to
-				return false
-			}
-		})
+    // Although .exec() is buffered for us,
+    // we'd have to escape everything we pass in. Boo.
+    var buffer: any[] = []
 
-		if (startNodePos === 0 && endNodePos === 0) {
-			new ErrorModal(this.app, "No code blocks found in this document.").open()
-			return
-		}
+    sfmt.stdin.write(content);
+    sfmt.stdin.end();
 
-		// Remove the backticks at start/end. Probably a nicer way to do this?
-		startNodePos += 3
-		endNodePos -= 3
+    sfmt.stderr.on('data', (chunk: any) => {
+      new ErrorModal(this.app, `${chunk}`).open()
+    })
 
-		let codeBlockContent = editorView.state.sliceDoc(startNodePos, endNodePos)
-		let formatted = await this.applyFormat(codeBlockContent)
+    sfmt.stdout.on('data', (chunk: any) => {
+      buffer.push(chunk)
+    });
 
-		editorView.dispatch({
-			changes: {
-				from: startNodePos,
-				to: endNodePos,
-				insert: formatted
-			}
-		})
-	}
+    return new Promise((resolve) => {
+      sfmt.on('close', (code: any) => {
+        console.log(`closed stream with code ${code}`)
+        resolve(`${buffer.join('')}`)
+      })
+    })
+  }
 
-	getExtension() {
-		this.getCodeBlocks()
-		this.registerEditorExtension(lintGutter())
-	}
+  async getCodeBlocks() {
 
-	registerLintDiagnostics() {
-		this.registerEditorExtension(
-			linter((editorView: EditorView) => {
+    const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+    if (view === null) {
+      return "null view"
+    }
+    const editorView: EditorView = (view.editor as any).cm as EditorView;
+    let cursor = syntaxTree(editorView.state).cursor()
 
-				let diagnostics: Diagnostic[] = []
-				let cursor = syntaxTree(editorView.state).cursor()
-				cursor.iterate((node: any) => {
+    // Would use null, but TS still thinks its null even when we check.
+    var startNodePos: number = 0
+    var endNodePos: number = 0
 
-					// TODO: Show swiftlint errors inline :)
-					if (node.name.contains("HyperMD-codeblock-begin")) {
-						console.log(node)
-						diagnostics.push({
-							from: cursor.from,
-							to: cursor.to,
-							severity: "warning",
-							message: `${node}`,
-							actions: []
-						})
-					} else if (node.name.contains("HyperMD-codeblock-end")) {
-						console.log(node)
-						diagnostics.push({
-							from: cursor.from,
-							to: cursor.to,
-							severity: "warning",
-							message: `${node}`,
-							actions: []
-						})
-					}
-				})
+    // Find code blocks with the lang
+    // TODO: Filter to `swift`
+    cursor.iterate((node: SyntaxNodeRef) => {
+      if (node.name.contains("HyperMD-codeblock-begin")) {
+        startNodePos = node.node.from
+        return false
+      } else if (node.type.name.contains("HyperMD-codeblock-end")) {
+        endNodePos = node.node.to
+        return false
+      }
+    })
 
-				return diagnostics
-			}))
-	}
+    if (startNodePos === 0 && endNodePos === 0) {
+      new ErrorModal(this.app, "No code blocks found in this document.").open()
+      return
+    }
+
+    // Remove the backticks at start/end. Probably a nicer way to do this?
+    startNodePos += 3
+    endNodePos -= 3
+
+    let codeBlockContent = editorView.state.sliceDoc(startNodePos, endNodePos)
+    let formatted = await this.applyFormat(codeBlockContent)
+
+    editorView.dispatch({
+      changes: {
+        from: startNodePos,
+        to: endNodePos,
+        insert: formatted
+      }
+    })
+  }
+
+  getExtension() {
+    this.getCodeBlocks()
+    this.registerEditorExtension(lintGutter())
+  }
+
+  registerLintDiagnostics() {
+    this.registerEditorExtension(
+      linter((editorView: EditorView) => {
+
+        let diagnostics: Diagnostic[] = []
+        let cursor = syntaxTree(editorView.state).cursor()
+        cursor.iterate((node: any) => {
+
+          // TODO: Show swiftlint errors inline :)
+          if (node.name.contains("HyperMD-codeblock-begin")) {
+            // console.log(node)
+            diagnostics.push({
+              from: cursor.from,
+              to: cursor.to,
+              severity: "warning",
+              message: `${node}`,
+              actions: []
+            })
+          } else if (node.name.contains("HyperMD-codeblock-end")) {
+            // console.log(node)
+            diagnostics.push({
+              from: cursor.from,
+              to: cursor.to,
+              severity: "warning",
+              message: `${node}`,
+              actions: []
+            })
+          }
+        })
+
+        return diagnostics
+      }))
+  }
 
 }
 
 class ErrorModal extends Modal {
-	constructor(app: App, message: string) {
-		super(app);
-		const { contentEl } = this;
-		contentEl.setText(message);
-	}
+  constructor(app: App, message: string) {
+    super(app);
+    const { contentEl } = this;
+    contentEl.setText(message);
+  }
 
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
+  onClose() {
+    const { contentEl } = this;
+    contentEl.empty();
+  }
 }
